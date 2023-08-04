b0aeb300f0627865fb3fe4f73e3cec8b8108621b
flatline-01 <iknowthatiknownothing@tutanota.com>
2023-08-04 21:15:28 +0600

    save alerts in the database

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import schedule
 import time
+import psycopg
 from threading import Thread
 from binance.spot import Spot
 from telebot import TeleBot
@@ -12,6 +13,13 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = TeleBot(BOT_TOKEN)
 client = Spot(api_key=API_KEY, api_secret=API_SECRET)
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+connection = psycopg.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host='localhost')
+cursor = connection.cursor()
 
 
 @bot.message_handler(commands=['start'])
@@ -58,9 +66,16 @@ def get_price_check_delay(m, crypto_abbr, option, price):
 
 
 def run_scheduled_task(chat_id, crypto_abbr, option, price, delay):
+    save_alert(chat_id, crypto_abbr, option, price, delay)
     (schedule.every(delay)
      .minutes.do(compare_prices, chat_id=chat_id, crypto_abbr=crypto_abbr, option=option, price=price))
     Thread(target=schedule_checker).start()
+
+
+def save_alert(tg_id, crypto_abbr, option, price, delay):
+    cursor.execute('INSERT INTO alerts (currency_abbr, price, telegram_id, delay, option) VALUES (%s,%s,%s,%s,%s)',
+                   (crypto_abbr, price, tg_id, delay, option, ))
+    connection.commit()
 
 
 def schedule_checker():
